8c21fc738c2f359b1767728985adf25622847e03
Rati Gvazbaia <rgvazbaia@gmail.com>
2022-12-02 16:22:11 +0400

    add script

---
+++ script.js
@@ -0,0 +1,79 @@
+// davaleba 1
+
+// let obj = {
+//     Berlin: 'Germany' ,
+//    Tbilisi: 'Georgia' ,
+//    Kiev: 'Ukraine' 
+//    };
+
+//    for( let Tbilisi in obj){
+//     console.log(Tbilisi+" is "+obj[Tbilisi]);
+//    };
+   
+
+// davaleba 2
+
+//    let a=4;
+//    while(a<100){
+//         ++a;
+//        console.log(a);
+//    }
+   
+
+// davaleba 3 
+// let array1= [1, 2, 4, -1, -3, 8, 7, 0, -5, 18, 12];
+//  for (let numb of array1 ){
+//     if (numb > 0 && numb < 10)          
+//     console.log(numb);
+//  }
+
+
+// davaleba 4
+// let array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for (let numb of array2){
+//     if (numb==5)            
+//     console.log('aris');
+    
+// }
+
+// davaleba 5 
+// let array3= [1, 2, 3, 4, 5];
+// let sum = 0;
+// for (let numb = 0; numb < array3.length; numb +=1){
+//     sum += array3[numb];   
+// }
+// console.log(sum);
+
+
+// davaleba 6
+
+// let array4 = [1, 2, 3, 7, 6, 9];
+// let sum = 0;
+// for (let numb = 0; numb < array4.length; numb +=1){
+//     sum += array4[numb];
+// }
+// console.log(sum/array4.length);
+
+
+// davaleba 7
+// let i=20;
+// switch(i) {
+//         case 5:
+//             console.log('ricxvi aris 5');
+//         break;
+//         case 10:
+//             console.log('ricxvi aris 10');
+//         break;
+//         default:
+//             console.log('sxva ricxvi');
+// }
+
+
+// davaleba 8
+// let array5 = [1, 2, 3, 7, 6, 9];
+// for (let numb of array5){
+//     if( numb == 7){
+//         continue;
+//     }
+//     console.log(numb)
+// }
